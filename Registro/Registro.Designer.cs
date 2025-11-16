71a5b2cf36ffefa6eb405d5fc492f20d31379f4e
Kitsune <jorgeoso41@hotmail.com>
2025-11-15 20:33:24 -0500

    Registro
    Cambiar la conexion

---
+++ Registro/Registro.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace Registro
+{
+    partial class Registro
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.Registrarbtn = new System.Windows.Forms.Button();
+            this.Volverbtn = new System.Windows.Forms.Button();
+            this.Contraseñatxt = new System.Windows.Forms.TextBox();
+            this.Confirmtxt = new System.Windows.Forms.TextBox();
+            this.Usuariotxt = new System.Windows.Forms.TextBox();
+            this.Cerrarbtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(359, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(223, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Registro de usuario";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(156, 173);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(194, 24);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Confirmar contraseña:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(229, 118);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(111, 24);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Contraseña:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(261, 76);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(79, 24);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Usuario:";
+            // 
+            // Registrarbtn
+            // 
+            this.Registrarbtn.Location = new System.Drawing.Point(372, 249);
+            this.Registrarbtn.Name = "Registrarbtn";
+            this.Registrarbtn.Size = new System.Drawing.Size(162, 47);
+            this.Registrarbtn.TabIndex = 15;
+            this.Registrarbtn.Text = "Registrar";
+            this.Registrarbtn.UseVisualStyleBackColor = true;
+            this.Registrarbtn.Click += new System.EventHandler(this.Registrarbtn_Click);
+            // 
+            // Volverbtn
+            // 
+            this.Volverbtn.Location = new System.Drawing.Point(135, 249);
+            this.Volverbtn.Name = "Volverbtn";
+            this.Volverbtn.Size = new System.Drawing.Size(162, 47);
+            this.Volverbtn.TabIndex = 16;
+            this.Volverbtn.Text = "Volver a Login";
+            this.Volverbtn.UseVisualStyleBackColor = true;
+            this.Volverbtn.Click += new System.EventHandler(this.Volverbtn_Click);
+            // 
+            // Contraseñatxt
+            // 
+            this.Contraseñatxt.Location = new System.Drawing.Point(372, 123);
+            this.Contraseñatxt.Name = "Contraseñatxt";
+            this.Contraseñatxt.Size = new System.Drawing.Size(210, 20);
+            this.Contraseñatxt.TabIndex = 20;
+            // 
+            // Confirmtxt
+            // 
+            this.Confirmtxt.Location = new System.Drawing.Point(372, 173);
+            this.Confirmtxt.Name = "Confirmtxt";
+            this.Confirmtxt.Size = new System.Drawing.Size(210, 20);
+            this.Confirmtxt.TabIndex = 21;
+            // 
+            // Usuariotxt
+            // 
+            this.Usuariotxt.Location = new System.Drawing.Point(372, 76);
+            this.Usuariotxt.Name = "Usuariotxt";
+            this.Usuariotxt.Size = new System.Drawing.Size(210, 20);
+            this.Usuariotxt.TabIndex = 23;
+            // 
+            // Cerrarbtn
+            // 
+            this.Cerrarbtn.Location = new System.Drawing.Point(595, 249);
+            this.Cerrarbtn.Name = "Cerrarbtn";
+            this.Cerrarbtn.Size = new System.Drawing.Size(162, 47);
+            this.Cerrarbtn.TabIndex = 27;
+            this.Cerrarbtn.Text = "Cerrar";
+            this.Cerrarbtn.UseVisualStyleBackColor = true;
+            this.Cerrarbtn.Click += new System.EventHandler(this.Cerrarbtn_Click);
+            // 
+            // Registro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 409);
+            this.Controls.Add(this.Cerrarbtn);
+            this.Controls.Add(this.Usuariotxt);
+            this.Controls.Add(this.Confirmtxt);
+            this.Controls.Add(this.Contraseñatxt);
+            this.Controls.Add(this.Volverbtn);
+            this.Controls.Add(this.Registrarbtn);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Name = "Registro";
+            this.Text = "Registro";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button Registrarbtn;
+        private System.Windows.Forms.Button Volverbtn;
+        private System.Windows.Forms.TextBox Contraseñatxt;
+        private System.Windows.Forms.TextBox Confirmtxt;
+        private System.Windows.Forms.TextBox Usuariotxt;
+        private System.Windows.Forms.Button Cerrarbtn;
+    }
+}
+
---
+++ Registro/Registro.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+using System.Text.RegularExpressions;
+
+namespace Registro
+{
+    public partial class Registro : Form
+    {
+        public Registro()
+        {
+            InitializeComponent();
+
+          
+            Contraseñatxt.UseSystemPasswordChar = true;
+            Confirmtxt.UseSystemPasswordChar = true;
+        }
+
+        
+        private bool ValidarPassword(string password)
+        {
+            string pattern = @"^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*(),.?""{}|<>]).{6,}$";
+            return Regex.IsMatch(password, pattern);
+        }
+
+        private void Registrarbtn_Click(object sender, EventArgs e)
+        {
+            string usuario = Usuariotxt.Text.Trim();
+            string clave = Contraseñatxt.Text.Trim();
+            string confirmar = Confirmtxt.Text.Trim();
+
+            // VALIDACIONES ------------------------------
+            if (usuario == "" || clave == "" || confirmar == "")
+            {
+                MessageBox.Show("Todos los campos son obligatorios", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (clave != confirmar)
+            {
+                MessageBox.Show("Las contraseñas no coinciden", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (!ValidarPassword(clave))
+            {
+                MessageBox.Show("La contraseña debe contener:\n" +
+                                "- Al menos 1 letra mayúscula\n" +
+                                "- Al menos 1 número\n" +
+                                "- Al menos 1 carácter especial\n" +
+                                "- Mínimo 6 caracteres",
+                                "Contraseña débil",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                using (var conn = Conexion.GetConnection())
+                {
+                    conn.Open();
+
+                    string query = "SELECT public.registrar_cliente(@p_usuario, @p_clave);";
+
+                    using (var cmd = new NpgsqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("p_usuario", usuario);
+                        cmd.Parameters.AddWithValue("p_clave", clave);
+
+                        var result = cmd.ExecuteScalar();
+
+                        MessageBox.Show("Cliente registrado correctamente",
+                                        "Éxito",
+                                        MessageBoxButtons.OK,
+                                        MessageBoxIcon.Information);
+
+                        // limpiar campos
+                        Usuariotxt.Clear();
+                        Contraseñatxt.Clear();
+                        Confirmtxt.Clear();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al registrar cliente:\n" + ex.Message,
+                                "Error",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Error);
+            }
+        }
+
+        private void Cerrarbtn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Volverbtn_Click(object sender, EventArgs e)
+        {
+            //aqui colocar el form de inicio de sesion
+        }
+    }
+}
